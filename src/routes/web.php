c2e0a0e2a5135d0eab294bd73629bd02065574f4
Nicu <nicu.plesa@net4media.net>
2026-02-23 15:01:02 +0200

    wip

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,6 +6,5 @@ use nplesa\InfinityGrid\Http\Controllers\GridController;
 Route::prefix('infinitygrid')
     ->name('infinitygrid.')
     ->group(function () {
-        Route::get('/data', [GridController::class, 'index'])
-            ->name('data');
+        Route::get('/data', [GridController::class, 'index'])->name('data');
     });
